79009e56c99137598afe993ae35227b2efb011de
phischu <pschuster@uni-koblenz.de>
2014-06-09 17:29:45 +0200

    threade fixity application throught class default signatures

--- src/Language/Haskell/Exts/Fixity.hs
+++ src/Language/Haskell/Exts/Fixity.hs
@@ -221,6 +221,7 @@ instance AppFixity Annotation where
 
 instance AppFixity ClassDecl where
     applyFixities fixs (ClsDecl decl) = liftM ClsDecl $ applyFixities fixs decl
+    applyFixities fixs (ClsDefSig loc decl) = liftM (ClsDefSig loc) $ applyFixities fixs decl
     applyFixities _ cdecl = return cdecl
 
 instance AppFixity InstDecl where
